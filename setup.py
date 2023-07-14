f73744229130db0f9d9731befe48e46eba15fb1e
wummeke <wumswatchface@gmail.com>
2023-07-14 14:53:53 +0200

    updated version number

--- setup.py
+++ setup.py
@@ -7,14 +7,14 @@
 plugin_identifier = "slicerthumb"
 
 # The plugin's python package, should be "octoprint_<plugin identifier>", has to be unique
-plugin_package = "slicerthumb"
+plugin_package = "octoprint_slicerthumb"
 
 # The plugin's human readable name. Can be overwritten within OctoPrint's internal data via __plugin_name__ in the
 # plugin module
 plugin_name = "OctoPrint-Slicerthumb"
 
 # The plugin's version. Can be overwritten within OctoPrint's internal data via __plugin_version__ in the plugin module
-plugin_version = "0.1.1"
+plugin_version = "0.1.3"
 
 # The plugin's description. Can be overwritten within OctoPrint's internal data via __plugin_description__ in the plugin
 # module
